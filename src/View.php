a5a9e1b392ec668f01b0a13beb95b6869004e0b1
Alexander Isik <isik.alexander@yandex.ru>
2017-07-04 16:12:39 +0300

    Update View.php

--- src/View.php
+++ src/View.php
@@ -5,4 +5,22 @@ namespace Raice;
 class View extends Application
 {
 	
+    public function __construct ( $className )
+    {
+		
+        parent::__construct();
+		
+        $this->render( $className );
+        
+    }
+    
+    public function render ( $className )
+    {
+		
+        require ROOT_PATH . 'protected/Views/header.php';
+        require ROOT_PATH . 'protected/Views/' . $className . '/' . $className . '.php';
+        require ROOT_PATH . 'protected/Views/footer.php';
+        
+    }
+    
 }
