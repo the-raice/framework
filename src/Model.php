e0c87effb45ba15804bca3c42248bb2dd3b5abe4
Alexander Isik <isik.alexander@yandex.ru>
2017-07-05 06:03:48 +0300

    Update Model.php

--- src/Model.php
+++ src/Model.php
@@ -5,4 +5,18 @@ namespace Raice;
 class Model
 {
     
+    /**
+     * The Database object
+     *
+     * @var object
+     */
+    protected $database;
+    
+    public function __construct ()
+    {
+        
+        $this->database = new Database();
+        
+    }
+    
 }
