69d94fb2369f4c8c3f641807981d16055b7ddc26
Alexander Isik <isik.alexander@yandex.ru>
2017-07-05 05:27:38 +0300

    Update Session.php

--- src/Session.php
+++ src/Session.php
@@ -4,5 +4,35 @@ namespace Raice;
 
 class Session
 {
-  
+    
+    public static function init ()
+    {
+        
+        session_start();
+        
+    }
+        
+    public static function set ( $key, $value )
+    {
+        
+        return $_SESSION[$key] = $value;
+        
+    }
+    
+    public static function get ( $key )
+    {
+        
+        return $_SESSION[$key];
+        
+    }
+    
+    public static function destroy ()
+    {
+        
+        unset( $_SESSION );
+        
+        session_destroy();
+        
+    }
+    
 }
