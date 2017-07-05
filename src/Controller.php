8fc172670c82acdaab69e6a740ebf64b62ab186e
Alexander Isik <isik.alexander@yandex.ru>
2017-07-05 06:11:01 +0300

    Update Controller.php

--- src/Controller.php
+++ src/Controller.php
@@ -5,4 +5,34 @@ namespace Raice;
 class Controller
 {
     
+    /**
+     * The View object
+     *
+     * @var object
+     */
+    public $view;
+    
+	/**
+	 * The name of class
+	 *
+	 * @var string
+	 */
+    public $className;
+	
+    public function __construct ( $className )
+    {
+        
+        $this->view = new View( $className );
+        
+    }
+    
+    public function getName()
+    {
+        
+        $this->className = substr( strrchr( static::class, "\\" ), 1);    
+		
+        return $this->className;
+        
+    }
+    
 }
