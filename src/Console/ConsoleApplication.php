35d5f075952bbb860d95401a88c13a4a6eeb348f
Alexander Isik <isik.alexander@yandex.ru>
2017-07-05 06:36:55 +0300

    Update ConsoleApplication.php
    Create the ConsoleApplication class

--- src/Console/ConsoleApplication.php
+++ src/Console/ConsoleApplication.php
@@ -1,1 +1,16 @@
+<?php
 
+/**
+ * The Raice Framework console.
+ *
+ * @link 
+ * @copyright Copyright (c) 2017 The Raice Framework
+ * @license 
+ */
+
+namespace Raice\Console;
+
+class ConsoleApplication 
+{
+  
+}
