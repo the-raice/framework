db5aa0d1dbba28ea30746217815fa54caa25e4c8
Alexander Isik <isik.alexander@yandex.ru>
2017-12-07 13:57:49 +1000

    Update Bootstrap.php

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -91,6 +91,80 @@ class Bootstrap
         if ( !empty( $this->method ) ) {
             
             $this->methodName = $this->controllerName . '\\' . $this->method ;
+    
+            if ( method_exists( $this->controllerName, $this->method ) ) {
+                
+                $this->bootstrap = new $this->controllerName;
+                
+                if ( !empty( $this->argument ) ) {
+                    
+                   $this->bootstrap->{$this->method}( $this->argument );
+                    
+                } else {
+
+                  $this->bootstrap->{$this->method}();
+                    
+                }
+                
+            } else {
+				
+				$this->bootstrap = new $this->controllerName;
+				
+			}
+          
+        } else {
+            
+            $this->bootstrap = new $this->controllerName;
+            
+        }
+        
+    }
+
+}
+
+    /**
+     * The Bootstrap object
+     *
+     * @var object
+     */
+    protected $bootstrap;
+    
+    /**
+     * Full name of the controller
+     *
+     * @var string
+     */
+    protected $controllerName;
+    
+    /**
+     * Full name of the method
+     *
+     * @var string
+     */
+    protected $methodName;
+    
+    public function __construct ( )
+    {
+
+        $this->url = explode('/', $_SERVER['REQUEST_URI']);
+        
+        $this->controller = ucfirst( $this->url[1] );
+        $this->method = ucfirst( $this->url[2] );
+        $this->argument = $this->url[3];
+        
+        if ( empty( $this->controller ) ) {
+            
+            $this->controllerName = '\Controllers\Home';
+            
+        } else {
+            
+            $this->controllerName = '\Controllers\\' . $this->controller;
+            
+        }
+
+        if ( !empty( $this->method ) ) {
+            
+            $this->methodName = $this->controllerName . '\\' . $this->method ;
             
             /*if ( class_exists( $this->methodName ) ) {
                 
